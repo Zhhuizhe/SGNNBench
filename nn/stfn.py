7a2d9fe37045008b19f2aadc2448187d91d5b71a
zhuiz <zhanghzh33@mail2.sysu.edu.cn>
2025-03-27 16:58:23 +0800

    Update nn

--- nn/stfn.py
+++ nn/stfn.py
@@ -6,28 +6,69 @@ from torch_geometric.nn import Linear, GCNConv, GATConv
 from .neuron import STLIF, LIF, reset_net
 
 
-class STFNorm(nn.Module):
-    def __init__(self, in_channels: int, v_threshold: float=1.0):
-        super().__init__()
-        self.v_threshold = v_threshold
-        self.lin = Linear(in_channels, in_channels, bias=True, weight_initializer='glorot')
-        self.reset_parameters()
+class STFNorm(nn.BatchNorm1d):
+    def __init__(
+        self, 
+        num_features: int, 
+        v_th: float,
+        eps: float=1e-05,
+        momentum: float=0.1,
+        affine: bool=True,
+        track_running_stats: bool=True,
+        device=None,
+        dtype=None
+    ) -> None:
+        factory_kwargs = {'device': device, 'dtype': dtype}
+        super().__init__(
+            num_features, eps, momentum, affine, track_running_stats, **factory_kwargs
+        )
+        self.num_features = num_features
+        self.v_th = v_th
+        self.running_mean = torch.reshape(self.running_mean, (1, 1, -1))
+        self.running_var = torch.reshape(self.running_var, (1, 1, -1))
     
-    def reset_parameters(self):
-        self.lin.reset_parameters()
-
-    def forward(self, spike: Tensor):
-            assert spike.ndim == 3
-            T, N, d = spike.shape
-            out = spike[-1, :, :]
+    def forward(self, input: Tensor) -> Tensor:
+        self._check_input_dim(input)
+        
+        if self.momentum is None:
+            exponential_average_factor = 0.0
+        else:
+            exponential_average_factor = self.momentum
             
-            spike = spike.transpose(0, 1).reshape(N, T*d) # N×T×d
-            E = torch.mean(spike, dim=-1, keepdim=True)
-            Var = torch.std(spike, dim=-1, keepdim=True)
-            
-            out = self.v_threshold * (out - E) / (Var + 1e-8)
-            return self.lin(out)
+        if self.training and self.track_running_stats:
+            if self.num_batches_tracked is not None:
+                self.num_batches_tracked.add_(1)
+                if self.momentum is None:  # use cumulative moving average
+                    exponential_average_factor = 1.0 / float(self.num_batches_tracked)
+                else:  # use exponential moving average
+                    exponential_average_factor = self.momentum
 
+        if self.training:
+            mean = input.mean(dim=(0, 2), keepdim=True)
+            # use biased var in train
+            var = input.var(dim=(0, 2), unbiased=False, keepdim=True) # compute variance via the biased estimator (i.e. unbiased=False)
+            n = input.numel() / input.size(1)
+            with torch.no_grad():
+                num_nodes, num_features = input.size(1), input.size(2)
+                self.running_mean = self.running_mean.expand(1, num_nodes, num_features)
+                self.running_var = self.running_var.expand(1, num_nodes, num_features)
+
+                self.running_mean = exponential_average_factor * mean\
+                    + (1 - exponential_average_factor) * self.running_mean
+                # update running_var with unbiased var
+                self.running_var = exponential_average_factor * var * n / (n - 1)\
+                    + (1 - exponential_average_factor) * self.running_var
+        else:
+            mean = self.running_mean
+            var = self.running_var
+        
+        mean, var = mean.squeeze(0), var.squeeze(0)
+        input = self.v_th * (input - mean) / (torch.sqrt(var + self.eps))
+        if self.affine: # if True, we use the affine transformation (linear transformation)
+            input = input * self.weight[None, None, :] + self.bias[None, None, :]
+
+        return input[-1, :, :]
+      
 
 class STFNConv(nn.Module):
     def __init__(
@@ -75,8 +116,8 @@ class STFN(nn.Module):
         else:
             raise NotImplementedError
 
-        self.norm1 = STFNorm(hid_channels, hid_channels)
-        self.norm2 = STFNorm(out_channels, out_channels)
+        self.norm1 = STFNorm(hid_channels, v_threshold)
+        self.norm2 = STFNorm(out_channels, v_threshold)
 
         self.snn1 = LIF(v_threshold=v_threshold)
         self.snn2 = LIF(v_threshold=v_threshold)
